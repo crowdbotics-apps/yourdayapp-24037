c416d80a79f2c169f6debc8a83a9a80c610240c1
Crowdbotics <team@crowdbotics.com>
2021-01-25 02:09:40 +0000

    add package blueprint-react-native-dashboard-2-screen

--- src/redux/mainReducer.js
+++ src/redux/mainReducer.js
@@ -6,6 +6,7 @@ import { combineReducers } from "redux";
 
 
 //@BlueprintReduxImportInsertion
+import CopyOfDashboard210196194Reducer from '../features/CopyOfDashboard210196194/redux/reducers'
 import Dashboard210196193Reducer from '../features/Dashboard210196193/redux/reducers'
 import Dashboard19196192Reducer from '../features/Dashboard19196192/redux/reducers'
 import Dashboard18196191Reducer from '../features/Dashboard18196191/redux/reducers'
@@ -20,6 +21,7 @@ export const combinedReducers = combineReducers({
 
 
   //@BlueprintReduxCombineInsertion
+CopyOfDashboard210196194: CopyOfDashboard210196194Reducer,
 Dashboard210196193: Dashboard210196193Reducer,
 Dashboard19196192: Dashboard19196192Reducer,
 Dashboard18196191: Dashboard18196191Reducer,
