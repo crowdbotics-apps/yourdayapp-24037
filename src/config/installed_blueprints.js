a550539e4ec47a3e1049accef5c657149ec1e947
Crowdbotics <team@crowdbotics.com>
2021-01-25 13:50:37 +0000

    add package blueprint-react-native-settings-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'CopyOfCopyOfSettings13196379', human_name: 'Copy of Copy of Settings 13', access_route: 'CopyOfCopyOfSettings13196379'},
 { name: 'CopyOfSettings13196378', human_name: 'Copy of Settings 13', access_route: 'CopyOfSettings13196378'},
 { name: 'CopyOfSettings13196377', human_name: 'Copy of Settings 13', access_route: 'CopyOfSettings13196377'},
 { name: 'CopyOfSettings13196376', human_name: 'Copy of Settings 13', access_route: 'CopyOfSettings13196376'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import CopyOfCopyOfSettings13196379Navigator from '../features/CopyOfCopyOfSettings13196379/navigator';
 import CopyOfSettings13196378Navigator from '../features/CopyOfSettings13196378/navigator';
 import CopyOfSettings13196377Navigator from '../features/CopyOfSettings13196377/navigator';
 import CopyOfSettings13196376Navigator from '../features/CopyOfSettings13196376/navigator';
@@ -26,6 +27,7 @@ import SignIn42196185Navigator from '../features/SignIn42196185/navigator';
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+CopyOfCopyOfSettings13196379: { screen: CopyOfCopyOfSettings13196379Navigator },
 CopyOfSettings13196378: { screen: CopyOfSettings13196378Navigator },
 CopyOfSettings13196377: { screen: CopyOfSettings13196377Navigator },
 CopyOfSettings13196376: { screen: CopyOfSettings13196376Navigator },
